77d59343167cd4889297f1b79bb8e2c067843e3e
gf <gf@gf.net>
2017-07-05 23:43:12 +0100

    [bigdata-1] A library for holding a real real large integer.

---
+++ src/main/java/net/gf/BigData.java
@@ -0,0 +1,187 @@
+package net.gf;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map.Entry;
+
+public class BigData implements Entry<Integer, BigData> {
+	private static final int BASE = 10;
+		
+	public static BigData build(final long input) {
+		long value = input;
+		final BigData ret = new BigData(0);
+		BigData data = null;
+		int bit = 0;
+		
+		while (value != 0) {
+			bit = (int)(value % BASE);
+			value = (long)(value/BASE);
+			
+			if (data == null) {
+				ret.value = bit;
+				data = ret;
+			} else {
+				final BigData next = new BigData(bit);
+				data.next = next;
+				
+				data = next;
+			}
+		}
+		
+		return ret ;
+	}
+	
+	public static BigData build(final int input) {
+		return build((long)input);
+	}
+	
+	private int value = 0;
+	private BigData next = null;
+	
+	private BigData(){
+	}
+	
+	private BigData(int bit) {
+		this.value = bit;
+	}
+	
+	private BigData(int bit, BigData next) {
+		this.value = bit;
+		this.next = next;
+	}
+	
+	public BigData add (final BigData other) {
+		int reminder = 0;
+		
+		int v = value + other.value;
+		if (v >= BASE) {
+			v = v - BASE;
+			reminder = 1;
+		}
+				
+		final BigData ret = new BigData(v);
+		BigData data = ret;
+		
+		BigData myNext = next;
+		BigData oNext = other.next;
+		
+		do {
+			v = reminder;
+			
+			if (myNext != null) {
+				v += myNext.value;
+				myNext = myNext.next;
+			}
+			
+			if (oNext != null) {
+				v += oNext.value;
+				oNext = oNext.next;
+			}
+
+			if (v >= BASE) {
+				v = v - BASE;
+				reminder = 1;
+			} else 
+				reminder = 0;
+
+			data = add(data, v);
+
+		} while (myNext != null || oNext != null);
+	
+		if (reminder == 1) {
+			data = add(data, reminder);
+		}
+		
+		return ret;
+	}
+	
+	private BigData add(BigData data, int v) {
+		final BigData nNext = new BigData(v);
+		
+		data.next = nNext;
+			
+		return nNext;
+	}
+	
+	@Override
+	public String toString(){
+		final StringBuilder ret = new StringBuilder(String.valueOf(value));
+		BigData n = next;
+		
+		while(n != null) {
+			ret.append(n.value);
+			n = n.next;
+		}
+		
+		return ret.reverse().toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((next == null) ? 0 : next.hashCode());
+		result = prime * result + value;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BigData other = (BigData) obj;
+		if (next == null) {
+			if (other.next != null)
+				return false;
+		} else if (!next.equals(other.next))
+			return false;
+		if (value != other.value)
+			return false;
+		return true;
+	}
+	
+	public BigData reverse() {
+		BigData ret = new BigData(value);
+		BigData n = next;
+		
+		while (n != null) {
+			final BigData tmp = new BigData(n.value, n.next);
+			
+			n = n.next;
+		
+			tmp.next = ret;
+			
+			ret = tmp;
+		}
+			
+		return ret;
+	}
+	
+	public void write(OutputStream out) throws IOException {
+		BigData reverse = reverse();
+		
+		out.write(reverse.value + 48);
+		
+		while (reverse.next != null) {
+			reverse = reverse.next;
+			
+			out.write(reverse.value + 48);
+		}
+	}
+
+	public Integer getKey() {
+		return value;
+	}
+
+	public BigData getValue() {
+		return next;
+	}
+
+	public BigData setValue(BigData value) {
+		return next = value;
+	}
+}
---
+++ src/test/java/net/gf/BigDataTest.java
@@ -0,0 +1,102 @@
+package net.gf;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class BigDataTest {
+
+	@Test
+	public void build1Test() {
+		long value = 65123;
+
+		BigData data = BigData.build(value);
+
+		assertEquals(value, Long.parseLong(data.toString()));
+
+		value = Long.MAX_VALUE;
+		data = BigData.build(value);
+
+		assertEquals(value, Long.parseLong(data.toString()));
+	}
+
+	@Test
+	public void addTest() {
+		long value1 = 19528l;
+		long value2 = 23222923l;
+
+		BigData data1 = BigData.build(value1);
+		BigData data2 = BigData.build(value2);
+
+		BigData data = data1.add(data2);
+
+		assertEquals(value1 + value2, Long.parseLong(data.toString()));
+
+		value1 = 99528l;
+		value2 = 99899l;
+
+		data1 = BigData.build(value1);
+		data2 = BigData.build(value2);
+
+		data = data1.add(data2);
+
+		assertEquals(value1 + value2, Long.parseLong(data.toString()));
+
+		value1 = 12l;
+		value2 = 23222923l;
+
+		data1 = BigData.build(value1);
+		data2 = BigData.build(value2);
+
+		data = data1.add(data2);
+
+		assertEquals(value1 + value2, Long.parseLong(data.toString()));
+	}
+	
+	@Test
+	public void build2Test() {
+		long value = Long.MAX_VALUE;
+
+		BigData data = BigData.build(value);
+		
+		System.out.println(data);
+		
+		data = data.add(BigData.build(99));
+		
+		System.out.println(data);
+		assertEquals("9223372036854775906", data.toString());
+	}
+	
+	@Test
+	public void build3Test() {
+		long value = Long.MAX_VALUE;
+
+		BigData data = BigData.build(value);
+				
+		data = data.add(BigData.build(value));
+		
+		System.out.println(data);
+		assertEquals("18446744073709551614", data.toString());
+	}
+	
+	@Test
+	public void reverseTest() throws IOException {
+		long value = Long.MAX_VALUE;
+
+		BigData data = BigData.build(value);
+		System.out.println(data);
+		System.out.println(data.reverse());
+		System.out.println(data);
+		
+		DataOutputStream file  = new DataOutputStream(new FileOutputStream("./tmp"));
+		
+		data.write(file);
+		file.flush();
+		
+		file.close();
+	}
+}
